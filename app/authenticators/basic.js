7ec1879d32495d654e1582c5317962309c3b3d67
Agata Andrzejewska <agatamonikaandrzejewska@gmail.com>
2016-12-13 01:45:56 +0100

    wrap run-loop failing test code

--- app/authenticators/basic.js
+++ app/authenticators/basic.js
@@ -18,48 +18,54 @@ export default Base.extend({
   store: Ember.inject.service(),
 
   session: Ember.inject.service('session'),
-
+  
   async authenticate (userid, password) {
-    // first we create the base64 encoded string of userid:password
-    let inBase64 = window.btoa(decodeURIComponent(encodeURIComponent(`${userid}:${password}`)));
-    // we unload all the store - to clear the cache
-    this.get('store').unloadAll();
+      Ember.run( async function() {
+        // first we create the base64 encoded string of userid:password
+        let inBase64 = window.btoa(decodeURIComponent(encodeURIComponent(`${userid}:${password}`)));
+        // we unload all the store - to clear the cache
+        this.get('store').unloadAll();
 
-    try {
-      // authenticate simply - to test the validity of the credentials
-      console.log('00000000000000000000');
-      await this.get('session')
-        .authenticate('authenticator:basic-simple', userid, password, inBase64);
-      console.log('1111111111111111111');
+        try {
+          // authenticate simply - to test the validity of the credentials
+          console.log('00000000000000000000');
+          await this.get('session')
+            .authenticate('authenticator:basic-simple', userid, password, inBase64);
+          console.log('1111111111111111111');
 
-      // query the database for the user
-      let result = await this.get('store')
-        .findRecord('user', userid, { reload: true });
+          // query the database for the user
+          let result = await this.get('store')
+            .findRecord('user', userid, { reload: true });
+          
+          console.log(result, '&&&&&&&&&&&&&&&&&');
+          console.log(Michal);
+          // invalidate the simple session
+          await this.get('session').invalidate();
 
-      console.log(result, '&&&&&&&&&&&&&&&&&');
+          // check whether the user is logged in
+          if (result.get('description') === undefined) {
+            throw new Error('The credentials are wrong');
+          }
 
-      // invalidate the simple session
-      await this.get('session').invalidate();
+          return {
+            basicCredentials: inBase64,
+            username: userid,
+            verified: !!result.get('email')
+          };
 
-      // check whether the user is logged in
-      if (result.get('description') === undefined) {
-        throw new Error('The credentials are wrong');
+        } catch (e) {
+          console.log('^^^^^^^^^^^^^^^^^^', e);
+          return this.get('session').invalidate().then(() => {
+            // console.error(e);
+            throw new Error('Authentication was not successful');
+          });
+        }
+
       }
-
-      return {
-        basicCredentials: inBase64,
-        username: userid,
-        verified: !!result.get('email')
-      };
-
-    } catch (e) {
-      console.log('^^^^^^^^^^^^^^^^^^', e);
-      return this.get('session').invalidate().then(() => {
-        // console.error(e);
-        throw new Error('Authentication was not successful');
-      });
-    }
+    );
   },
+
+
 
   restore (data) {
     const { basicCredentials, username } = data;
@@ -74,7 +80,12 @@ export default Base.extend({
   invalidate() {
     this.get('store').unloadAll();
     return Promise.resolve();
+  
+  
   },
+
+  
+  
 
 });
 
